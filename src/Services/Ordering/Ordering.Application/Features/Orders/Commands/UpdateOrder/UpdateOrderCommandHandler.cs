6cf4e10ae0da2be195aba8632c784b76316a9edb
Mdilow <25sekhwelad@gmail.com>
2022-03-09 20:56:27 +0200

    Developed Application Command Features - Update Order

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandhandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandhandler> _logger;
+
+        public UpdateOrderCommandhandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandhandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            // Update order
+          var orderToUpdate= await  _orderRepository.GetByIdAsync(request.Id);
+            if (orderToUpdate==null)
+            {
+                _logger.LogError("Order does not exist on database.");
+               // throw new NotFoundException(nameof(Order), request.Id);
+            }
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpdate);
+
+            _logger.LogError($"Order {orderToUpdate.Id} is successfully updated");
+
+            return Unit.Value;
+        }
+    }
+}
