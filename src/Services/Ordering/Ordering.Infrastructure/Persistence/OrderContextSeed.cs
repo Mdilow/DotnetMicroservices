e8268c14476d6fe2df4effe83eada917ed6dc4ed
Mdilow <25sekhwelad@gmail.com>
2022-03-10 22:00:18 +0200

    Developed Infrastructure Layer Persistence

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext,ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName="delight",FirstName="Delight",LastName="Sekhwela",EmailAddress="25sekhwelad@gmail.com", AddressLine="Bolobedu", Country="South Africa" , TotalPrice=350}
+            };
+        }
+    }
+}
